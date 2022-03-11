57753851c2c228b40f235d0f1c377e725896b4ea
Chhorvy Om <vivi@chhorvys-imac.attlocal.net>
2022-03-10 22:26:22 -0800

    Task 3: Use dot notation and index to log specific object and its key/value pair

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log(reviews[5].feedback);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 (not auto-tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
